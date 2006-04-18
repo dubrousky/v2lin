449b6fdd63ebb4efeafa9734b748ad0ef99d6617
conan_sh <conan_sh@8865ccd7-bd0e-0410-9ca4-8e41419f945a>
2006-04-18 10:56:17 +0000

    big reoder and new added new functions, version 0.2

--- branches/conan_hacker/lib/vxw_defs.h
+++ branches/conan_hacker/lib/vxw_defs.h
@@ -1,40 +1,37 @@
-/*****************************************************************************
- * vxw_defs.h - declares the error message and other constants needed
- *              to implement a Wind River VxWorks (R) kernel API 
- *              in a POSIX Threads environment.
- *  
- * Copyright (C) 2000, 2001  MontaVista Software Inc.
- *
- * Author : Gary S. Robertson
- *
+/****************************************************************************
+ * Copyright (C) 2004, 2005, 2006 v2lin Team <http://v2lin.sf.net>
+ * Copyright (C) 2000,2001  Monta Vista Software Inc.
+ * 
+ * This file is part of the v2lin Library.
  * VxWorks is a registered trademark of Wind River Systems, Inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * 
+ * Initial implementation Gary S. Robertson, 2000, 2001.
+ * Contributed by Andrew Skiba, skibochka@sourceforge.net, 2004.
+ * Contributed by Mike Kemelmakher, mike@ubxess.com, 2005.
+ * Contributed by Constantine Shulyupin, conan.sh@gmail.com, 2006.
+ * 
+ * The v2lin library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * The v2lin Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
  ****************************************************************************/
 
-#if __cplusplus
-extern "C" {
-#endif
-
+__BEGIN_DECLS;
 /*
 **  Generic data types
 */
 #ifndef BOOL
 #define BOOL int
 #endif
-
 #ifndef STATUS
 #define STATUS int
 #endif
-
 /*
 **  Miscellaneous error codes
 */
@@ -44,20 +41,14 @@ extern "C" {
 #define OBJ_ERRS                        0x003d0000
 #define SEM_ERRS                        0x00160000
 #define SM_OBJ_ERRS                     0x00580000
-
 #define S_memLib_NOT_ENOUGH_MEMORY      (MEM_ERRS + 1)
-
 #define S_msgQLib_INVALID_MSG_LENGTH    (MSGQ_ERRS + 1)
-
 #define S_objLib_OBJ_DELETED            (OBJ_ERRS + 3)
 #define S_objLib_OBJ_ID_ERROR           (OBJ_ERRS + 1)
 #define S_objLib_OBJ_TIMEOUT            (OBJ_ERRS + 4)
 #define S_objLib_OBJ_UNAVAILABLE        (OBJ_ERRS + 2)
-
 #define S_semLib_INVALID_OPERATION      (SEM_ERRS + 0x00000068)
-
 #define S_smObjLib_NOT_INITIALIZED      (SM_OBJ_ERRS + 1)
-
 #define S_taskLib_ILLEGAL_PRIORITY      (TASK_ERRS + 0x00000065)
 
 /*
@@ -65,7 +56,6 @@ extern "C" {
 */
 #define NO_WAIT                         0
 #define WAIT_FOREVER                    -1
-
 /*
 **  Message Queue Option Flags
 */
@@ -73,7 +63,6 @@ extern "C" {
 #define MSG_PRI_URGENT                  0x01
 #define MSG_Q_FIFO                      0x00
 #define MSG_Q_PRIORITY                  0x01
-
 /*
 **  Semaphore Option Flags
 */
@@ -84,7 +73,7 @@ extern "C" {
 #define SEM_DELETE_SAFE                 0x04
 #define SEM_INVERSION_SAFE              0x08
 
-#if __cplusplus
-}
-#endif
 
+#include <pthread.h>
+
+__END_DECLS;
