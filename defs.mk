5253bfb2f17b3abb9b3433254f3e85215cc4f516
conan_sh <conan_sh@8865ccd7-bd0e-0410-9ca4-8e41419f945a>
2006-04-20 15:51:38 +0000

    fixed compilation on other platforms

--- defs.mk
+++ defs.mk
@@ -4,4 +4,5 @@ CFLAGS += -g
 CFLAGS += -D _USR_SYS_INIT_KILL
 CFLAGS += -Wall -Wno-format -Wno-unused-label -Wno-unused-variable
 CFLAGS += -fmessage-length=0
+CFLAGS+=-fPIC 
 #CFLAGS+=-DDEBUG # use for the global debugging
